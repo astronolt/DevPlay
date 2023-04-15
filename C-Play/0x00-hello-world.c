11f2943d8fda9ac166920df694dbb6d050029ff4
astronolt <odinaka.dev@gmail.com>
2023-04-15 15:52:42 +0100

    stage: variables

---
+++ C-Play/0x00-hello-world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num = 10;
+	float decimalOne = 1.1;
+	char letter = 'c';
+
+	printf("Hello world\n");
+	printf("This is a number(%d) and a character(%c) and a decimal(%f) and a rounded-up by 2 digits decimal(%.2f)\n", num, letter, decimal, decimal);
+	return (0);
+}
